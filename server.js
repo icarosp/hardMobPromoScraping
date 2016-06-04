947e2d671564e5774b270c893c1baa471f03ee34
Icaro F. Guselian <icaro.ifg@gmail.com>
2016-06-04 11:10:33 -0300

    Enabled CORS

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var cheerio = require('cheerio');
 var app     = express();
 var cloudscraper = require('cloudscraper');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/promos', function (request, response) {
 
 	var json = [];
